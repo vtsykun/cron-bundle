842c9d05a4ecfe65c8162bbfc700f35660a77f14
Vladimir Tsykun <vtsykun@okvpn.org>
2020-01-17 04:17:18 +0300

    Bugfix

--- src/Middleware/CronMiddlewareEngine.php
+++ src/Middleware/CronMiddlewareEngine.php
@@ -23,7 +23,7 @@ final class CronMiddlewareEngine implements MiddlewareEngineInterface
     public function handle(ScheduleEnvelope $envelope, StackInterface $stack): ScheduleEnvelope
     {
         if (!$stamp = $envelope->get(ScheduleStamp::class)) {
-            $stack->next()->handle($envelope, $stack);
+            return $stack->next()->handle($envelope, $stack);
         }
 
         if (CronExpression::factory($stamp->cronExpression())->isDue('now', $this->timeZone)) {
